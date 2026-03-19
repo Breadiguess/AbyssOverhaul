92cd9b6d4824f3e3bcbedb71a17a53a94a5e303c
pil <harry.eglin@outlook.com>
2026-03-19 21:00:59 +0000

    start of jackhammer

---
+++ Content/Items/Weapons/Melee/ImpactHammer/ImpactHammer.cs
@@ -0,0 +1,92 @@
+﻿using AbyssOverhaul.Content.Rarities;
+using CalamityMod;
+using CalamityMod.CustomRecipes;
+using CalamityMod.Dusts;
+using CalamityMod.Items;
+using CalamityMod.Items.BaseItems;
+using CalamityMod.Items.Materials;
+using CalamityMod.Particles;
+using CalamityMod.Projectiles.Melee;
+using CalamityMod.Projectiles.Typeless;
+using CalamityMod.Rarities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using rail;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AbyssOverhaul.Content.Items.Weapons.Melee.ImpactHammer
+{
+    public class ImpactHammer : ModProjectile, ILocalizedModType
+    {
+        public ref float TimeAlive => ref Projectile.ai[1];
+        public ref Player Owner => ref Main.player[Projectile.owner];
+        public static string Path => ModContent.GetInstance<ImpactHammer>().GetPath();
+        public new string LocalizationCategory => "Items.Weapons.Melee";
+
+        public int ShakingLevel = 0;
+        public static Asset<Texture2D> HeadTex;
+        public static Asset<Texture2D> ArmTex;
+        public enum State
+        {
+            Idle,
+            Charging,
+            Hitting,
+        }
+        public State CurrentState = State.Idle;
+
+        public override void Load()
+        {
+            HeadTex = ModContent.Request<Texture2D>($"{ImpactHammer.Path}Head");
+            ArmTex = ModContent.Request<Texture2D>($"{ImpactHammer.Path}Arm");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 40;
+            Projectile.height = 24; 
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.ContinuouslyUpdateDamageStats = true;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            return base.PreDraw(ref lightColor);
+        }
+        public override void OnSpawn(IEntitySource source) 
+        {
+            base.OnSpawn(source);
+        }
+        public override void AI()
+        {
+            TimeAlive++;
+            DoPlayerCheck();
+            Vector2 TruePosition = Owner.Center;
+            Projectile.rotation = Projectile.rotation.AngleLerp(Projectile.Center.AngleTo(Owner.Calamity().mouseWorld), 0.5f);
+
+            if (ShakingLevel > 0)
+            {
+                Projectile.position = TruePosition + new Vector2(ShakingLevel * 0.5f, 0).RotatedByRandom(Math.PI * 2);
+            }
+            else
+            {
+                Projectile.position = TruePosition;
+            }
+
+        }
+
+        private void DoPlayerCheck()
+        {
+            if (Owner.HeldItem.type == ModContent.ItemType<ImpactHammerItem>())
+            {
+                Projectile.timeLeft = 2;
+            }
+        }
+    }
+}
---
+++ Content/Items/Weapons/Melee/ImpactHammer/ImpactHammerItem.cs
@@ -0,0 +1,76 @@
+﻿using AbyssOverhaul.Content.Rarities;
+using CalamityMod;
+using CalamityMod.CustomRecipes;
+using CalamityMod.Dusts;
+using CalamityMod.Items;
+using CalamityMod.Items.BaseItems;
+using CalamityMod.Items.Materials;
+using CalamityMod.Particles;
+using CalamityMod.Projectiles.Melee;
+using CalamityMod.Projectiles.Typeless;
+using CalamityMod.Rarities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AbyssOverhaul.Content.Items.Weapons.Melee.ImpactHammer
+{
+    public class ImpactHammerItem : ModItem, ILocalizedModType
+    {
+        public static string Path => ModContent.GetInstance<ImpactHammerItem>().GetPath();
+        public new string LocalizationCategory => "Items.Weapons.Melee";
+        public override void SetDefaults()
+        {
+            Item.width = 52;
+            Item.height = 36;
+            Item.damage = 50;
+            Item.DamageType = DamageClass.Melee;
+            Item.useAnimation = 20;
+            Item.useTime = 20;
+            Item.useTurn = true;
+            Item.knockBack = 12f;
+            Item.autoReuse = true;
+            Item.value = CalamityGlobalItem.RarityTurquoiseBuyPrice;
+            Item.rare = ModContent.RarityType<AbyssalRarity>();
+            Item.useStyle = ItemUseStyleID.MowTheLawn;
+            Item.channel = true;
+            Item.shoot = ModContent.ProjectileType<ImpactHammer>();
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+        }
+        public override bool MeleePrefix() => true;
+        //public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0; not sure if this is needed
+        public override void HoldItem(Player player)
+        {
+            if (player.ownedProjectileCounts[Item.shoot] <= 0 && player.HeldItem.type == ModContent.ItemType<ImpactHammerItem>())
+            {
+                Projectile.NewProjectile(player.GetSource_FromThis(), player.Center, Vector2.Zero, ModContent.ProjectileType<ImpactHammer>(), 0, 0);
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<MysteriousCircuitry>(12)
+                .AddIngredient<DubiousPlating>(12)
+                //.AddIngredient(ItemID. TIER-APPROPRIATE MATERIAL ,12)
+                //.AddCondition(ArsenalTierGatedRecipe.ConstructRecipeCondition( THE NEW TIER THAT WE MIGHT ADD, out Func<bool> condition), condition)
+                .AddTile(TileID.MythrilAnvil);
+        }
+      
+        
+    }
+}
