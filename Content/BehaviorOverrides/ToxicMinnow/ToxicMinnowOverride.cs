1afe562cc002c45ba93ee538c5ee1312548a1856
Nathaniel <76820008+Support-Crispy@users.noreply.github.com>
2026-04-24 01:00:51 -0400

    Toxic Minnow Override Base

---
+++ Content/BehaviorOverrides/ToxicMinnow/ToxicMinnowOverride.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbyssOverhaul.Content.BehaviorOverrides.ToxicMinnow
+{
+    internal class ToxicMinnowOverride : NPCBehaviorOverride, IEcologyParticipant
+    {
+        public override int NPCType => ModContent.npcType<ToxicMinnow>();
+    }
+}
